5ea28580d4b39ee56e74dba3c07171e828e69b87
Brisbane Web Developer <brisbanewebdeveloper@outlook.com>
2022-05-20 00:58:09 +1000

    Added the install script

---
+++ install-custom-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TMPDIR=/tmp/AutoRaise
+INSTALLDIR=/opt/homebrew/opt/autoraise/bin/
+
+mkdir -p $TMPDIR && \
+echo "Created a temporal directory $TMPDIR" && \
+cd $TMPDIR && \
+git clone git@github.com:brisbanewebdeveloper/AutoRaise.git -b 77-make-it-work-with-bartender-custom . && \
+make clean && make CXXFLAGS=-DALTERNATIVE_TASK_SWITCHER && \
+echo "Overwritting the existing version in $INSTALLDIR" && \
+echo "(You are going to be asked your login password)" && \
+chmod 555 AutoRaise && \
+sudo ditto AutoRaise $INSTALLDIR && \
+cd - && \
+rm -fr $TMPDIR && \
+echo "Removed the temporal directory $TMPDIR" && \
+brew services restart autoraise && \
+echo "All Done!"
